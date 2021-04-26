5f099e0fd9c4e5ce55d3d1167a49d07aa531c98b
amartin58 <adee1001@outlook.com>
2021-04-26 16:38:14 -0700

    Install node prompt module

--- cashRegister.js
+++ cashRegister.js
@@ -1,3 +1,6 @@
+const ps = require("prompt-sync");
+const prompt = ps();
+
 
 function startProgram(){
 const transOptions = new Map()
@@ -9,11 +12,18 @@ transaction1 = transOptions.get(1)
 transaction2 = transOptions.get(2)
 transaction3 = transOptions.get(3)
 
-console.log(`~~~ Welcome to Wally's Hotdogs ~~~ \n${transaction1} \n${transaction2} \n${transaction3}`)
-
+return `~~~ Welcome to Wally's Hotdogs ~~~ \n${transaction1} \n${transaction2} \n${transaction3}`
 
 }
+
 console.log(startProgram())
 
+function makeTransSelection(){
+  let transelection = prompt('Please enter your selection: ')
+// console.log(`${transelection}`)
+if (transelecton === 1){
+  console.log('hi')
+}
+}
 
-
+console.log(makeTransSelection())
