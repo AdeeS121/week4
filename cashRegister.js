44fcabe1d3f62f219b7d5f096e13b51212cd57be
amartin58 <adee1001@outlook.com>
2021-04-27 04:37:58 -0700

    Work on option choices

--- cashRegister.js
+++ cashRegister.js
@@ -11,9 +11,19 @@ let transaction1 = transOptions.get(1)
 let transaction2 = transOptions.get(2)
 let transaction3 = transOptions.get(3)
 
+const choicePayOptions = new Map()
+choicePayOptions.set(1, '[1] - Hot Dog - $4.50')
+choicePayOptions.set(2, '[2] - Soda - $1.50')
+choicePayOptions.set(3, '[3] - Chips - $1.00')
+choicePayOptions.set(4, '[4] - Start Payment Process')
+let item1 = choicePayOptions.get(1)
+let item2 = choicePayOptions.get(2)
+let item3 = choicePayOptions.get(3)
+let item4 = choicePayOptions.get(4)
+
 welcome = welcomeMsg(transaction1, transaction2, transaction3)
 console.log(welcome)
-getChoice = displayChoices(transaction1, transaction2, transaction3)
+getChoice = displayChoices(item1, item2, item3, item4)
 console.log(getChoice)
 
 }
@@ -23,23 +33,30 @@ console.log(`~~~ Welcome to Wally's Hotdogs ~~~ \n${transaction1} \n${transactio
 // prompt('Please make a selection: ') 
 userSelection = prompt('Please make a selection: ')
 
-if (userSelection == 1){ 
-console.log(displayChoices()) 
-// console.log(userSelection)
-} else {
-    console.log('Choose another code')
-}
+
 }
 
-// function to show food options if transacton 1 choosen
-function displayChoices() {
+// show food options with option to start payment
+// add object map for selecting items
+function displayChoices(item1, item2, item3, item4) {
+
+    if (userSelection == 1){ 
+        console.log('Hi') 
+        // console.log(userSelection)
+        } else {
+            console.log('Choose another code')
+        }
+    
+    
+//    console.log(`${item1} \n${item2} \n${item3} \n${item4}`)
+   prompt('Select an item: ') 
+//    userSelection = prompt('Please make a selection: ')
+
     // if (transaction1 === 1){
     //  console.log('testerbester')   
     // } else {
         // console.log('failed test')
     // }
-
-
 }
 
 
