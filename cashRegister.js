e1eb241a3c55a0c8fdb9b39094ca1d587375b0eb
amartin58 <adee1001@outlook.com>
2021-05-03 16:47:53 -0700

    Update selections

--- cashRegister.js
+++ cashRegister.js
@@ -6,6 +6,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+
 function startProgram() {
   const transOptions = new Map();
   transOptions.set(1, "[1] - Start transaction");
@@ -14,6 +15,7 @@ function startProgram() {
   transOptions.set(3, "[3] - Exit");
   let transaction1 = transOptions.get(1);
   let transaction2 = transOptions.get(2);
+
   let transaction3 = transOptions.get(3);
 
   const choicePayOptions = new Map();
@@ -26,24 +28,59 @@ function startProgram() {
   let item3 = choicePayOptions.get(3);
   let item4 = choicePayOptions.get(4);
 
-  let getTrans = selectTrans(transaction1, transaction2, transaction3);
-  console.log(getTrans);
-  let getChoice = selectItem(item1, item2, item3, item4);
-  console.log(getChoice);
+  // let getTrans = selectTrans(transaction1, transaction2, transaction3);
+  // console.log(getTrans);
+  // let getChoice = selectItem(item1, item2, item3, item4);
+  // console.log(getChoice);
 }
 
-function selectTrans(transaction1, transaction2, transaction3) {
-  return `~~~ Welcome to Wally\'s Hogdogs ~~~ \n\n${transaction1} \n${transaction2} \n${transaction3}`;
-}
 
-function selectItem(item1, item2, item3, item4) {
-  rl.question('Make selection: ', function (selection) {
-console.log(`${item1} ${item2} ${item3} ${item4}`)
-    // console.log(`${item1} ${item2} ${item3} ${item4}`);
-    // console.log('test')
-    // rl.close()
-  });
-}
+// return startProgram();
 
-return startProgram();
+// function is invoked one have answer from the terminal
+// answer invoked as an argument
+console.log('~~ Welcome to Wally\'s Hotdogs ~~')
+console.log('[1] - Start new transaction \n[2] - Current running transaction \n[3] - Exit');
+rl.question('Make selection: ', function(){
+  // if (selection = 1)
+  console.log('[1] - Hot Dog - $4.50 \n[2] - Soda - $1.50 \n[3] - Chips - $1.00 \n[4] - Start Payment Process');
+
+  rl.on('line', function (selection){
+  if (selection == 4) {
+    rl.close() 
+  } else if (selection == 1) {
+    console.log('~~ You added a Hot Dog ~~')
+  } else if (selection == 2){
+    console.log('~~ You added a Soda ~~')
+  } else if (selection ==3) {
+    console.log('You added Chips')
+  }
+  console.log('[1] - Hot Dog - $4.50 \n[2] - Soda - $1.50 \n[3] - Chips - $1.00 \n[4] - Start Payment Process');  
+
+  console.log('Select an item: ')
+});
+
+
+  
+})
+
+  
+//   rl.question('Select item: ', function() {
+// if (selection == 1) {
+//   console.log('~~ You added a Hot Dog ~~')
+//   rl.question('select again')
+//   // rl.close()
+// } else if (selection == 2) {
+//   console.log('~~ You added a Soda ~~')
+// } else if (selection == 3){
+//   console.log('You added Chips')
+// }
+
+//   })
+
+
+
+
+
+
 
