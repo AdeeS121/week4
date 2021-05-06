40850da48fd13a97ec0ca9d141052df054435582
amartin58 <adee1001@outlook.com>
2021-05-06 10:27:50 -0700

    Move object maps for global access, adjust functions

--- cashRegister.js
+++ cashRegister.js
@@ -5,59 +5,121 @@ const rl = readline.createInterface({
 });
 
 function startProgram() {
-  const transOptions = new Map();
-  transOptions.set(1, "[1] - Start transaction");
-  transOptions.set(2, "[2] - Current running transaction");
-  transOptions.set(3, "[3] - Exit");
-  // let transaction1 = transOptions.get(1);
-  // let transaction2 = transOptions.get(2);
-  // let transaction3 = transOptions.get(3);
-
-  const choicePayOptions = new Map();
-  choicePayOptions.set(1, "[1] - Hot Dog - $4.50");
-  choicePayOptions.set(2, "[2] - Soda - $1.50");
-  choicePayOptions.set(3, "[3] - Chips - $1.00");
-  choicePayOptions.set(4, "[4] - Start Payment Process");
-  // let item1 = choicePayOptions.get(1);
-  // let item2 = choicePayOptions.get(2);
-  // let item3 = choicePayOptions.get(3);
-  // let item4 = choicePayOptions.get(4);
-
   greetUser();
-  displayTransOptions(transOptions)
-  displayOptions(choicePayOptions)
-  displayUserChoice()
+  displayTransOptions();
+  displayChoicePayOptions()
 }
 
+const transOptions = new Map();
+transOptions.set('[1] - ', 'Start transaction');
+transOptions.set('[2] - ', 'Current running transaction');
+transOptions.set('[3] - ', 'Exit');
 
+const choicePayOptions = new Map();
+choicePayOptions.set('[1] - Hot Dog - ', '$4.50');
+choicePayOptions.set('[2] - Soda - ', '$1.50');
+choicePayOptions.set('[3] - Chips - ', '$1.00');
+choicePayOptions.set('[4]', 'Start Payment Process');
 
 function greetUser() {
-console.log('Welcome to Wally\'s HotDogs ');
+  console.log("Welcome to Wally's HotDogs");
 }
 
-function displayTransOptions(transOptions) {
-  for (list of transOptions)
-console.log(list)
-
+function displayTransOptions() {
+  for (let [key, value] of transOptions) {
+    console.log(key + value);
+   
+ rl.close();
+  }
 }
 
-function displayOptions(choicePayOptions) {
-  for (choice of choicePayOptions)
-console.log(choice)
+function displayChoicePayOptions() {
+  for (let [key, value] of choicePayOptions) {
+    console.log(key + value);
+    rl.close();
+  }
 }
 
-function displayUserChoice() {
 
-}
-
-
-
-// function displayChoice
-
+// greetUser();
+// displayTransOptions();
+// displayChoicePayOptions();
 startProgram()
 
 
 
+
+
+
+
+
+
+
+// startProgram()
+// add to the array
+//   choicePayOptions.set('Cookies', 50)
+//   console.log(choicePayOptions)
+
+//  // get value of the key(Hot Dog)
+//   console.log(choicePayOptions.get('Hot Dog'))
+
+//   // can delete an element by its key
+//   choicePayOptions.delete('Cookies')
+//   console.log(choicePayOptions)
+
+// const transOptions = new Map();
+// transOptions.set(1, "[1] - Start transaction");
+// transOptions.set(2, "[2] - Current running transaction");
+// transOptions.set(3, "[3] - Exit");
+
+// function startProgram() {
+//   const transOptions = new Map();
+//   transOptions.set(1, "[1] - Start transaction");
+//   transOptions.set(2, "[2] - Current running transaction");
+//   transOptions.set(3, "[3] - Exit");
+//   // let transaction1 = transOptions.get(1);
+//   // let transaction2 = transOptions.get(2);
+//   // let transaction3 = transOptions.get(3);
+
+//   const choicePayOptions = new Map();
+//   choicePayOptions.set(1, "[1] - Hot Dog - $4.50");
+//   choicePayOptions.set(2, "[2] - Soda - $1.50");
+//   choicePayOptions.set(3, "[3] - Chips - $1.00");
+//   choicePayOptions.set(4, "[4] - Start Payment Process");
+//   // let item1 = choicePayOptions.get(1);
+//   // let item2 = choicePayOptions.get(2);
+//   // let item3 = choicePayOptions.get(3);
+//   // let item4 = choicePayOptions.get(4);
+
+//   // greetUser();
+//   // displayTransOptions(transOptions)
+//   // displayOptions(choicePayOptions)
+//   // displayUserChoice()
+// }
+//
+
+// function greetUser() {
+// console.log('Welcome to Wally\'s HotDogs ');
+// }
+
+// function displayTransOptions(transOptions) {
+//   for (list in transOptions)
+// console.log('${list')
+
+// }
+
+// function displayOptions(choicePayOptions) {
+//   for (choice in choicePayOptions)
+// console.log(choice)
+// }
+
+// function displayUserChoice() {
+
+// }
+
+// function displayChoice
+
+// startProgram()
 
 // function greetUser(transaction1, transaction2, transaction3) {
 //   console.log(`Welcome to Wally's HotDogs \n\n${transaction1} \n${transaction2} \n${transaction3}`);
@@ -65,7 +127,7 @@ startProgram()
 
 // function selectItem(item1, item2, item3, item4) {
 //   rl.question('\nMake selection: ', function (selection) {
-  
+
 //     if (selection == 1) {
 //       console.log(`\n~~ Starting New Transaction ~~ \n\n${item1} \n${item2} \n${item3} \n${item4}`);
 //       console.log(`\nSelect an Item: `);
@@ -89,27 +151,20 @@ startProgram()
 //           } else if (item == 2){
 //             console.log(`\nSelect an Item: ${item}`);
 //             console.log(`~~ You added a Soda ~~ \n\n${item1} \n${item2} \n${item3} \n${item4}`);
-//               console.log('Select an Item: '); 
+//               console.log('Select an Item: ');
 //           } else if (item ==3) {
-//             console.log(`\nSelect an Item: ${item}`) 
+//             console.log(`\nSelect an Item: ${item}`)
 //             console.log(`~~ You added Chips ~~ \n\n${item1} \n${item2} \n${item3} \n${item4}`);
-//             console.log('Select an Item: '); 
+//             console.log('Select an Item: ');
 
 //           } else {
 //            rl.close()
-//           }         
-//     })  
+//           }
+//     })
 //   })
 // }
-
-
 
 // rl.on("close", function () {
 //   console.log("\nBYE BYE !!!");
 //   process.exit(0);
 // });
-
-
-
-
-
