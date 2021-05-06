d638406467777de4d267ff6e35fdce60ae9a69f0
amartin58 <adee1001@outlook.com>
2021-05-06 14:19:01 -0700

    Adjust function control flow

--- cashRegister.js
+++ cashRegister.js
@@ -7,53 +7,102 @@ const rl = readline.createInterface({
 function startProgram() {
   greetUser();
   displayTransOptions();
-  displayChoicePayOptions()
+  // displayChoicePayOptions();
 }
 
 const transOptions = new Map();
-transOptions.set('[1] - ', 'Start transaction');
-transOptions.set('[2] - ', 'Current running transaction');
-transOptions.set('[3] - ', 'Exit');
+transOptions.set("[1] - ", "Start transaction");
+transOptions.set("[2] - ", "Current running transaction");
+transOptions.set("[3] - ", "Exit");
 
 const choicePayOptions = new Map();
 choicePayOptions.set('[1] - Hot Dog - ', '$4.50');
 choicePayOptions.set('[2] - Soda - ', '$1.50');
 choicePayOptions.set('[3] - Chips - ', '$1.00');
-choicePayOptions.set('[4]', 'Start Payment Process');
+choicePayOptions.set('[4] - ', 'Start Payment Process');
 
 function greetUser() {
   console.log("Welcome to Wally's HotDogs");
 }
 
 function displayTransOptions() {
-  for (let [key, value] of transOptions) {
-    console.log(key + value);
+  for (let [key, value] of transOptions) 
+  console.log(key + value);
+  rl.question('\nMake selection: ', function (selection) {
+
+    if (selection == 1) {
+   displayChoicePayOptions()
+    }
+    else if (selection == 2) {
+     currentTransactions()
+    } else if (selection == 3) {
+      rl.close();
+    } else {
+     console.log('Invalid entry');
+    }
    
- rl.close();
-  }
+  })
 }
+
+ //   rl.on('line', function (item) {
+    //       if (selection == 5) {
+    //         rl.close()
+    //       } else if (item == 1) {
+    //       console.log(`\nSelect an Item: ${item}`)
+    //          console.log(`~~ You added a Hot Dog ~~ \n\n${item1} \n${item2} \n${item3} \n${item4}`);
+    //          console.log('Select an Item: ')
+
+    //       } else if (item == 2){
+    //         console.log(`\nSelect an Item: ${item}`);
+    //         console.log(`~~ You added a Soda ~~ \n\n${item1} \n${item2} \n${item3} \n${item4}`);
+    //           console.log('Select an Item: ');
+    //       } else if (item ==3) {
+    //         console.log(`\nSelect an Item: ${item}`)
+    //         console.log(`~~ You added Chips ~~ \n\n${item1} \n${item2} \n${item3} \n${item4}`);
+    //         console.log('Select an Item: ');
+
+    //       } else {
+    //        rl.close()
+    //       }
+    // })
 
 function displayChoicePayOptions() {
-  for (let [key, value] of choicePayOptions) {
-    console.log(key + value);
-    rl.close();
-  }
+  for (let [key, value] of choicePayOptions) 
+  console.log(key + value);
+
+}
+
+function currentTransactions() {
+    console.log('currentTransactions')
 }
 
 
-// greetUser();
-// displayTransOptions();
+
+
+// function displayChoicePayOptions() {
+
+// for (let [key, value] of choicePayOptions) 
+//   console.log(key + value);
+
+//  rl.question("Selection an item: ", function (choice) {
+
+//    if (choice == 1) {
+
+
+
+//  {
+      
+//     }
+//      console.log('Another successful try')
+//    }
+//   });
+// }
+
+// // greetUser();
+// // displayTransOptions();
 // displayChoicePayOptions();
-startProgram()
 
-
-
-
-
-
-
-
-
+startProgram();
 
 // startProgram()
 // add to the array
